ec334e1a4ade4649bd5f0a0fbf19b9f40551a477
Dương Song <duongsong9a4@gmail.com>
2023-12-16 19:36:35 +0700

    Make connection to db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,39 @@
 import express from "express";
 import cors from "cors";
+import mssql from "mssql"
+
 const app = express();
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+dotnev.config()
+
+var sql = mssql;
+
+var config = {
+  server: SERVER,
+  user: 'sa',
+  password: 'suyoung02',
+  database: 'DC_SYSTEM',
+  port: 1433,
+  options: {
+    trustServerCertificate: true
+  }
+}
+
+async function connectToDatabase() {
+  try {
+    await sql.connect(config);
+    console.log('Connected to the database');
+  } catch (err) {
+    console.error('Error connecting to the database:', err);
+  }
+}
+
+await connectToDatabase()
+
 app.listen(3000);
 console.log("Server running on port 3000");
+
